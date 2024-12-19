7f855d1ad659b3a6449de92eb6e3c6e3b803d102
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-18 22:24:06 -0300

    feat(design_system): Add design_system package for app components and theme

---
+++ design_system/lib/design_system.dart
@@ -0,0 +1,3 @@
+library design_system;
+
+export './src/image/image.dart';
---
+++ design_system/lib/src/image/app_image.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppImage {
+  static const String packageName = 'design_system';
+
+  static const arrows = AssetImage('assets/images/arrows.svg');
+}
