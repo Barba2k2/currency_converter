72ead6a3119ddb100ab965ce7fe6dc73bafd0d14
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-19 23:40:46 -0300

    fix(app_widget): Removed unused imports

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:design_system/design_system.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 import 'presentation/home.dart';
 
 class AppWidget extends StatelessWidget {
