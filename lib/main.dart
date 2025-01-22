61f665a80e84057259e0819695d6af99ddbaea5a
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-01-22 14:24:52 -0300

    feat(main): Add localizations and supoorted locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'src/app_widget.dart';
 import 'src/core/application_config.dart';
+import 'src/helpers/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +23,27 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // Inglês
+        Locale('pt'), // Português
+        // Locale('es'), // Espanhol
+      ],
+      localeResolutionCallback: (deviceLocale, supportedLocales) {
+        // Verifique se o idioma do dispositivo é suportado
+        for (var locale in supportedLocales) {
+          if (locale.languageCode == deviceLocale?.languageCode) {
+            return locale;
+          }
+        }
+        // Caso não seja suportado, retorna o inglês como padrão
+        return const Locale('en');
+      },
       home: const AppWidget(),
     );
   }
